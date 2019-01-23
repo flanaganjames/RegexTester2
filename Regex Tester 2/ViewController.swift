5fbb0f2956f3366de267a28b2bb5f1a8ef847102
Jim Flanagan <flanagan.james@gmail.com>
2019-01-23 06:59:12 -0700

    highlights matches

--- Regex Tester 2/ViewController.swift
+++ Regex Tester 2/ViewController.swift
@@ -4,17 +4,95 @@
 //
 //  Created by James Flanagan on 1/18/19.
 //  Copyright © 2019 James Flanagan. All rights reserved.
-//
+
+//  https://benscheirman.com/2014/06/regex-in-swift/
+//https://stackoverflow.com/questions/25525171/uitextview-highlight-all-matches-using-swift
+//http://www.thomashanning.com/uitableview-tutorial-for-beginners/
+
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource
+{
+ 
+    var countRegex : Int = 0
+    
+    private var data: [String] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countRegex
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")! //1.
+        
+        let text = data[indexPath.row] //2.
+        
+        cell.textLabel?.text = text //3.
+        
+        return cell
+    }
+    
 
+    @IBOutlet weak var regexObject: UITextField!
+    
+    @IBOutlet weak var targetObject: UITextView!
+    
+    @IBOutlet weak var resultObject: UILabel!
+    
+    @IBOutlet weak var tableObject: UITableView!
+    
+    var regexText : String = ""
+    var targetText : String = ""
+    
+    var error: NSError?
+    
+    var resultingMatches : NSTextCheckingResult? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        targetObject.layer.cornerRadius = 5
+        targetObject.layer.borderColor = UIColor.purple.cgColor
+        targetObject.layer.borderWidth = 1
+        
+        tableObject.dataSource = self
     }
 
-
+    @IBAction func regexChanged(_ sender: Any) {
+        regexText = regexObject.text!
+        targetText = targetObject.text!
+        let attributed = NSMutableAttributedString(string: targetObject.text)
+       
+        attributed.addAttribute(NSAttributedString.Key.font, value: UIFont(name: "HelveticaNeue", size: 28.0)!, range: NSMakeRange(0, targetText.count))
+        do {
+            
+            let regex =  try NSRegularExpression(pattern: regexText, options: .caseInsensitive)
+            
+            let resultingMatches = regex.matches(in: targetText, range:NSMakeRange(0, targetText.count))
+            
+            data.removeAll()
+            
+            
+            countRegex = resultingMatches.count
+            resultObject.text = "\(resultingMatches.count)"
+            
+            
+            for match in resultingMatches
+            {
+            attributed.addAttribute(NSAttributedString.Key.backgroundColor, value: UIColor.yellow, range: match.range)
+                
+                data.append(String(targetText[Range(match.range, in: targetText)!]))
+            }
+            targetObject.attributedText = attributed
+            
+            self.tableObject.reloadData()
+            
+        }
+        catch {
+            print(self.error)
+        }
+    }
+    
+    
 }
 
